57ad05d64c92c65ee46917e1d44fa026df0a515d
TopChainBoy <topchainboy@thedaoma.com>
2024-02-12 02:00:08 +0100

    refactor time reach to destination

--- galacticVoyager.sh
+++ galacticVoyager.sh
@@ -28,6 +28,34 @@ co2_footprints=(["foot"]=0 ["skateboard"]=0 ["bike"]=0 ["boat"]=0.15 ["car"]=0.1
 declare -A costs
 costs=(["foot"]=0 ["skateboard"]=0.01 ["bike"]=0.02 ["boat"]=0.5 ["car"]=0.1 ["train"]=0.2 ["plane"]=0.15 ["fighter jet"]=100 ["aurora x1"]=10000 ["spacex rocket"]=50000 ["speed of light"]=0 ["speed of light power of 10"]=0 ["speed of light power of 1000"]=0)
 
+# Function to convert time into the desired format
+convert_time() {
+  local time=$1
+  local seconds=$((time%60))
+  local minutes=$((time/60%60))
+  local hours=$((time/3600%24))
+  local days=$((time/86400%7))
+  local weeks=$((time/604800%4))
+  local months=$((time/2592000%12))
+  local years=$((time/31536000%10))
+  local decades=$((time/315360000%10))
+  local centuries=$((time/3153600000%10))
+  local millennia=$((time/31536000000))
+
+  # Only print time units that are not zero
+  [[ $millennia -ne 0 ]] && echo -n "$millennia millennia "
+  [[ $centuries -ne 0 ]] && echo -n "$centuries centuries "
+  [[ $decades -ne 0 ]] && echo -n "$decades decades "
+  [[ $years -ne 0 ]] && echo -n "$years years "
+  [[ $months -ne 0 ]] && echo -n "$months months "
+  [[ $weeks -ne 0 ]] && echo -n "$weeks weeks "
+  [[ $days -ne 0 ]] && echo -n "$days days "
+  [[ $hours -ne 0 ]] && echo -n "$hours hours "
+  [[ $minutes -ne 0 ]] && echo -n "$minutes minutes "
+  [[ $seconds -ne 0 ]] && echo -n "$seconds seconds"
+  echo
+}
+
 # Loop through the array and calculate the time for each mode of transportation
 for mode in "${keys[@]}"; do
   speed=${speeds[$mode]}
@@ -38,27 +66,19 @@ for mode in "${keys[@]}"; do
   echo -e "Mode of transportation: \033[1;34m$mode\033[0m"
   echo -e "Speed: \033[1;32m$speed km/h ($speed_mph mph)\033[0m"
   
-  # If time is less than 1 hour, convert it to minutes. If it's less than 1 minute, convert it to seconds. If it's less than 1 second, convert it to milliseconds.
-  if (( $(echo "$time < 1" | bc -l) )); then
-    time=$(echo "scale=2; $time * 60" | bc)
-    if (( $(echo "$time < 1" | bc -l) )); then
-      time=$(echo "scale=2; $time * 60" | bc)
-      if (( $(echo "$time < 1" | bc -l) )); then
-        time=$(echo "scale=2; $time * 1000" | bc)
-        echo -e "Time to reach destination: \033[1;33m$time milliseconds\033[0m"
-      else
-        echo -e "Time to reach destination: \033[1;33m$time seconds\033[0m"
-      fi
-    else
-      echo -e "Time to reach destination: \033[1;33m$time minutes\033[0m"
-    fi
+  # Convert time to seconds and then use the convert_time function
+  time_seconds=$(echo "$time * 3600" | bc | cut -f1 -d.)
+  if [[ $time_seconds -eq 0 ]]; then
+    echo -e "Time to reach destination: \033[1;33m0 milliseconds\033[0m"
   else
-    echo -e "Time to reach destination: \033[1;33m$time hours\033[0m"
+    time_converted=$(convert_time $time_seconds)
+    echo -e "Time to reach destination: \033[1;33m$time_converted\033[0m"
   fi
-  
   echo -e "CO2 footprint: \033[1;31m$co2 kg\033[0m"
   echo -e "Cost: \033[1;35m$cost dollars\033[0m"
   echo
+
+
 done
 echo "Note: Costs include energy, gas and food."
 echo "CO2 footprint and cost values are just approximations."
